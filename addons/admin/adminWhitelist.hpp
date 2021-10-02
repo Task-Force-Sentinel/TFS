1ed997190898f683f96a63ec61245c80b85b1ca4
YonV <32718226+YonVclaw@users.noreply.github.com>
2021-10-01 20:01:54 -0600

    after op fixes

--- addons/admin/adminWhitelist.hpp
+++ addons/admin/adminWhitelist.hpp
@@ -1,9 +1,13 @@
 
 cba_settings_whitelist[] = {
     "admin", // logged in/voted admin
-    admin_YONV
+    admin_YONV,
+    admin_storke,
+    admin_Ocean
 };
 
 enableDebugConsole[] = {
-    admin_YONV
+    admin_YONV,
+    admin_storke,
+    admin_Ocean
 };
