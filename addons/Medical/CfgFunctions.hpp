f77e3b19b870ba35567e012f8c0fc6a16fd398cd
YonV <32718226+YonVclaw@users.noreply.github.com>
2021-10-04 01:11:49 -0600

    medical folder fixs

--- addons/Medical/CfgFunctions.hpp
+++
@@ -1,9 +0,0 @@
-class TFS_Medical {
-    tag = "TFS";
-    class Medical {
-        file = "z\tfs\addons\medical\Functions";
-        class moduleMedicalFacility {};
-        class setMedicalFacility {};
-        class FacilityHeal {};
-    };
-};
